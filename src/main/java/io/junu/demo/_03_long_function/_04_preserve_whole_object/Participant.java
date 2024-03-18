a6bcbb8e64255056f80d2b4c9e1b5d6c1383b73a
kimjunwoo <junwoo1027@naver.com>
2024-03-19 00:48:49 +0900

    Preserve whole object

---
+++ src/main/java/io/junu/demo/_03_long_function/_04_preserve_whole_object/Participant.java
@@ -0,0 +1,22 @@
+package io.junu.demo._03_long_function._04_preserve_whole_object;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public record Participant(String username, Map<Integer, Boolean> homework) {
+    public Participant(String username) {
+        this(username, new HashMap<>());
+    }
+
+    public void setHomeworkDone(int index) {
+        this.homework.put(index, true);
+    }
+
+
+    double getRate(int studyDashboard) {
+        long count = homework().values().stream()
+                .filter(v -> v == true)
+                .count();
+        return (double) (count * 100 / studyDashboard);
+    }
+}
