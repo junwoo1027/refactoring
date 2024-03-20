6f074e78c9178d1fd6785205c51b2cc2d3f8133f
kimjunwoo <junwoo1027@naver.com>
2024-03-21 00:32:28 +0900

    반복문 쪼개기

---
+++ src/main/java/io/junu/demo/_03_long_function/_07_spilt_loop/StudyDashboard.java
@@ -0,0 +1,119 @@
+package io.junu.demo._03_long_function._07_spilt_loop;
+
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHIssueComment;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class StudyDashboard {
+
+    private final int totalNumberOfEvents;
+    private final List<Participant> participants;
+    private final Participant[] firstParticipantsForEachEvent;
+
+    public StudyDashboard(int totalNumberOfEvents) {
+        this.totalNumberOfEvents = totalNumberOfEvents;
+        this.participants = new CopyOnWriteArrayList<>();
+        this.firstParticipantsForEachEvent = new Participant[this.totalNumberOfEvents];
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        StudyDashboard studyDashboard = new StudyDashboard(15);
+        studyDashboard.print();
+    }
+
+    private void print() throws IOException, InterruptedException {
+        checkGithubIssues(getGhRepository());
+        new StudyPrinter(this.totalNumberOfEvents, this.participants).execute();
+        printFirstParticipants();
+    }
+
+    private void printFirstParticipants() {
+        Arrays.stream(this.firstParticipantsForEachEvent).forEach(p -> System.out.println(p.username()));
+    }
+
+    private static GHRepository getGhRepository() throws IOException {
+        GitHub gitHub = GitHub.connect();
+        GHRepository repository = gitHub.getRepository("whiteship/live-study");
+        return repository;
+    }
+
+    private void checkGithubIssues(GHRepository repository) throws InterruptedException {
+        ExecutorService service = Executors.newFixedThreadPool(8);
+        CountDownLatch latch = new CountDownLatch(this.totalNumberOfEvents);
+
+        for (int index = 1 ; index <= this.totalNumberOfEvents ; index++) {
+            int eventId = index;
+            service.execute(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        GHIssue issue = repository.getIssue(eventId);
+                        List<GHIssueComment> comments = issue.getComments();
+                        checkHomework(comments, participants, eventId);
+                        firstParticipantsForEachEvent[eventId - 1] = findFirst(comments, participants);
+                        latch.countDown();
+                    } catch (IOException e) {
+                        throw new IllegalArgumentException(e);
+                    }
+                }
+            });
+        }
+
+        latch.await();
+        service.shutdown();
+    }
+
+    private static Participant findFirst(List<GHIssueComment> comments, List<Participant> participants) throws IOException {
+        Date firstCreatedAt = null;
+        Participant first = null;
+        for (GHIssueComment comment : comments) {
+            Participant participant = findParticipant(comment.getUserName(), participants);
+            
+            if (firstCreatedAt == null || comment.getCreatedAt().before(firstCreatedAt)) {
+                firstCreatedAt = comment.getCreatedAt();
+                first = participant;
+            }
+        }
+        return first;
+    }
+
+    private static void checkHomework(List<GHIssueComment> comments, List<Participant> participants, int eventId) {
+        for (GHIssueComment comment : comments) {
+            Participant participant = findParticipant(comment.getUserName(), participants);
+            participant.setHomeworkDone(eventId);
+        }
+    }
+
+    private static Participant findParticipant(String username, List<Participant> participants) {
+        return isNewParticipant(username, participants) ?
+                createNewParticipant(username, participants) :
+                findExistingParticipant(username, participants);
+    }
+
+    private static Participant findExistingParticipant(String username, List<Participant> participants) {
+        Participant participant;
+        participant = participants.stream().filter(p -> p.username().equals(username)).findFirst().orElseThrow();
+        return participant;
+    }
+
+    private static Participant createNewParticipant(String username, List<Participant> participants) {
+        Participant participant;
+        participant = new Participant(username);
+        participants.add(participant);
+        return participant;
+    }
+
+    private static boolean isNewParticipant(String username, List<Participant> participants) {
+        return participants.stream().noneMatch(p1 -> p1.username().equals(username));
+    }
+}
