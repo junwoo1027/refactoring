483636a70ead835070c6b586554076d9fc1fff9e
kimjunwoo <junwoo1027@naver.com>
2024-03-24 22:58:00 +0900

    기능 편애

---
+++ src/main/java/io/junu/demo/_09_feature_envy/ElectricityUsage.java
@@ -0,0 +1,25 @@
+package io.junu.demo._09_feature_envy;
+
+public class ElectricityUsage {
+
+    private double amount;
+
+    private double pricePerUnit;
+
+    public ElectricityUsage(double amount, double pricePerUnit) {
+        this.amount = amount;
+        this.pricePerUnit = pricePerUnit;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public double getPricePerUnit() {
+        return pricePerUnit;
+    }
+
+    public double getElectricityBill() {
+        return this.amount * this.pricePerUnit;
+    }
+}
