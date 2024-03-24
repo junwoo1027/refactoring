ab40a3e80dd5aad79ee36516d5f3bdd127178cd6
kimjunwoo <junwoo1027@naver.com>
2024-03-25 02:21:02 +0900

    타입 코드를 서브 클래스로 바꾸기(direct inheritance)

---
+++ src/test/java/io/junu/demo/_11_primitive_obsession/_31_replace_type_code_with_subclasses/direct_inheritance/EmployeeTest.java
@@ -0,0 +1,15 @@
+package io.junu.demo._11_primitive_obsession._31_replace_type_code_with_subclasses.direct_inheritance;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeTest {
+
+    @Test
+    void employType() {
+        assertEquals("engineer", Employee.createEmployee("junwoo", "engineer").getType());
+        assertEquals("manager", Employee.createEmployee("junwoo", "manager").getType());
+        assertThrows(IllegalArgumentException.class, () -> Employee.createEmployee("junwoo", "wrong"));
+    }
+}
